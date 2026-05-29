9cacd865e460d82a834b168147bf55a1d8ed22f7
Dev-GustavoRibeiro <contatogustavoribeirohm@gmail.com>
2026-05-29 18:22:36 -0300

    feat: force exclusive fullscreen 1080p mode on game boot
    - Configured DisplayServer.window_set_mode to exclusive fullscreen inside global.gd _ready().
    - Updated project.godot to set window mode 4 (Exclusive Fullscreen) and enable borderless mode.

--- src/singletons/global.gd
+++ src/singletons/global.gd
@@ -3,7 +3,7 @@ var moedas = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
