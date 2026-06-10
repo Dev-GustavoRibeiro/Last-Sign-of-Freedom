7a70a35591702076c2efff9cc3118e9908f8b90b
JoaoMacedofsa <jpmfsa13@gmail.com>
2026-06-10 10:52:52 -0300

    Ajustando o que foi pedido na PR e a morte ao cair do mapa

--- src/components/hurtbox/hurt.gd
+++ src/components/hurtbox/hurt.gd
@@ -1,15 +1,4 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
-func _on_body_entered(_body: Node2D) -> void:
-	pass
+# Contato fisico com inimigos nao causa dano.
+# O player morre por tiro inimigo ou ao cair para fora do mundo.
